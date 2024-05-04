0dbcf079316c83940dc4bd8820a1102108a8299e
ShreyashSoni <yashsoni96@gmail.com>
2024-05-04 14:25:14 +0530

    fix logging issue

--- cybulde/utils/utils.py
+++ cybulde/utils/utils.py
@@ -1,10 +1,12 @@
 import socket
 import logging
 import subprocess
+logging.basicConfig(level=logging.NOTSET)
 
 
 def get_logger(name: str) -> logging.Logger:
-    return logging.Logger(f"[{socket.gethostname()}] {name}")
+    logger = logging.getLogger(f"[{socket.gethostname()}] {name}")
+    return logger
 
 def run_shell_command(cmd: str) -> str:
     return subprocess.run(cmd, text=True, shell=True, check=True, capture_output=True).stdout
